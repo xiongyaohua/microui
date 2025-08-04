10df7bb8026f1e7a421daaa26a243e1b02579280
Yaohua Xiong <xiongyaohua@gmail.com>
2025-08-04 09:39:06 +0800

    Add zls flake

--- flake.nix
+++ flake.nix
@@ -3,28 +3,43 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     zig = {
-      url = "github:mitchellh/zig-overlay";        
+      url = "github:mitchellh/zig-overlay";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-utils.follows = "flake-utils";
-      };  
+      };
+    };
+    zls = {
+      url = "github:zigtools/zls";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        zig-overlay.follows = "zig";
+      };
     };
   };
-  
-  outputs = { self, nixpkgs, flake-utils, zig }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        with pkgs;
-        {
-          devShells.default = mkShell {
-            buildInputs = [
-              zig.packages.${system}.master
-              sdl2-compat
-            ];
-          };
-        }
-      );
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      zig,
+      zls,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            zig.packages.${system}.master
+            zls.packages.${system}.default
+            sdl2-compat
+          ];
+        };
+      }
+    );
 }
